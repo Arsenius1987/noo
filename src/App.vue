81a0b5957c8a76a314e614ffe738e5f7ad47df28
Arsenius1987 <131542900+Arsenius1987@users.noreply.github.com>
2024-02-21 20:00:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import TemplateButton from './components/TemplateButton.vue'
+import CounterButton from './components/CounterButton.vue'
+import OptionsApi from './components/OptionsApi.vue'
+import LinksComponent from './components/LinksComponent.vue'
+import YesNoWTF from './components/YesNoWTF.vue'
+import {ref} from 'vue'
+const hide = ref(false)
+</script>
+
+<template>
+  <header>
+    <LinksComponent></LinksComponent>
+    <YesNoWTF></YesNoWTF>
+  </header>  
+  <CounterButton v-bind:start="10"/>
+  <CounterButton :start="15"/>
+  <CounterButton style="background:yellow"/>
+  <CounterButton class="myButton"/>
+  <CounterButton/>
+  <CounterButton/>
+  <CounterButton/>
+  <TemplateButton>Content from parent</TemplateButton>
+  <TemplateButton></TemplateButton>
+  <br>
+  <OptionsApi v-if="!hide" v-on:hide="hide=true"/>
+  <button @click="hide=false">Show hidden Button</button>
+</template>
+
+<style scoped>
+  .myButton {
+    background:red;
+  }
+</style>
---
+++ src/components/CounterButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+const props = defineProps(['start'])
+const start = props.start ? props.start : 0
+const counter = ref(start)
+const color = computed(()=>{
+  return counter.value%2?'blue':'orange'
+})
+</script>
+
+<template>
+  <button :style="'color:'+ color" class="innerStyle" @click="counter++">{{ counter }}</button>
+</template>
+
+<style scoped>
+.innerStyle {
+  background-color: green;
+}
+</style>
